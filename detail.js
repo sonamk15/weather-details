f9fc4bf81c0df23e7532098944b1458982e65770
Sonam kumawat <43805791+sonamk15@users.noreply.github.com>
2019-09-05 10:11:03 +0530

    Update detail.js

--- detail.js
+++ detail.js
@@ -1,6 +1,7 @@
 const app = require("./weather_details.js");
 const knex = require("./knexfile.js");
 const express = require("express");
+const axios  = require("axios");
 const app = express();
 app.use(express.json());
 
@@ -11,7 +12,7 @@ app.get('/',(request,response) => {
 
 let weather_data = express.Router();
 app.use('/weather_data',weather_data);
-require('./weather_details.js')(weather_data,knex);
+require('./weather_details.js')(weather_data,knex,axios);
 
 
 app.listen(3401,()=>{
